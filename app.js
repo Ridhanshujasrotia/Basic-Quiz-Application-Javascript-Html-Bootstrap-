390db3b4e3d03e571199f0fcfdd655a825377420
Ridhanshu jasrotia <100899931+Ridhanshujasrotia@users.noreply.github.com>
2023-02-23 00:55:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const correctAnswers = ["C", "B", "B", "B", "C", "B", "C", "D", "C", "C"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+    form.q5.value,
+    form.q6.value,
+    form.q7.value,
+    form.q8.value,
+    form.q9.value,
+    form.q10.value,
+  ];
+
+  //checking answers
+  userAnswers.forEach((answer, index) => {
+    if (answer == correctAnswers[index]) {
+      score += 10;
+    }
+  });
+
+  //displaying results
+  scrollTo(0, 0);
+
+  result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 60);
+});
+
+setTimeout(() => {
+  alert("Welcome, Folks!!");
+}, 2000);
